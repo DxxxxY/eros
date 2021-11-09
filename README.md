6e94932a5577d3a673682c50baaea665b55d46ab
DxxxxY <introsdream@gmail.com>
2021-11-09 12:18:25 -0500

    fix colors, complete contact form

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ Cleverly designed page for showcasing shoes.
 
 ## TODO:
 - [ ] Complete home page (model + text)
-- [ ] Complete contact page (revamp form)
 - [ ] Create guides page
 - [ ] Create media page
 - [ ] Create stores page
 - [ ] Reorganize js and css
-- [ ] Colors in dark and light mode
+- [ ] Color switch button
+- [ ] Fix underline animation color switch
+- [ ] Make unselected nav elements slightly darker and add hover
--- public/script.js
+++ public/script.js
@@ -2,8 +2,11 @@ let dark = true
 let i = 0
 setInterval(() => {
     if (i == 360) i = 0
-    document.body.style.backgroundColor = dark ? "#000000" : "#ffffff"
-    document.querySelectorAll("p, a, li, h2, h3, table, label, input, textarea, button").forEach(e => e.style.color = dark ? "#4a94fa" : "#990099")
+    document.querySelectorAll('*').forEach(e => {
+        e.style.color = dark ? "#4a94fa" : "#990099"
+        e.style.borderColor = dark ? "#4a94fa" : "#990099"
+        e.style.backgroundColor = dark ? "#000000" : "#ffffff"
+    })
     document.querySelectorAll("h1").forEach(e => {
         e.style.background = dark ? `linear-gradient(${i}deg, rgba(2, 0, 36, 1) 25%, rgba(2, 0, 36, 1) 50%, #4a94fa 75%, #4a94fa 100%)` : `linear-gradient(${i}deg, rgb(117, 22, 117) 25%, rgb(82, 41, 82) 50%, rgb(75, 23, 75) 75%, rgb(82, 41, 82) 100%)`
         e.style.webkitBackgroundClip = "text"
