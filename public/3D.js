b730bfd5c3e17e62f5d47cd41a416c367ced1611
DxxxxY <introsdream@gmail.com>
2021-11-28 02:49:26 -0500

    implement three.js, setup model, add credits

---
+++ public/3D.js
@@ -0,0 +1,55 @@
+//Imports
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.135.0/build/three.module.js"
+import { GLTFLoader } from "../utils/GLTFLoader.js"
+
+//Setup
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(0, 0, 0.3)
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canvas"), })
+renderer.setPixelRatio(window.devicePixelRatio)
+const loader = new GLTFLoader()
+let sneaker
+loader.load("../utils/model/sneaker/scene.gltf", e => {
+    scene.add(e.scene)
+    sneaker = e.scene
+    sneaker.rotation.set(0, radian(-90), radian(-45))
+}, null, e => { console.log(e) })
+
+// Torus
+// const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+// const material = new THREE.MeshStandardMaterial({ color: 0xff6347 })
+// const torus = new THREE.Mesh(geometry, material)
+// scene.add(torus)
+
+// Lights
+// const pointLight = new THREE.PointLight(0xffffff) //0x is #
+// pointLight.position.set(5, 5, 5)
+// const ambientLight = new THREE.AmbientLight(0xffffff) //0x is #
+// scene.add(pointLight, ambientLight)
+
+// Conversion
+const radian = (angle) => angle * (Math.PI / 180)
+
+// Scroll Animation
+document.body.onscroll = () => {
+    const t = document.body.getBoundingClientRect().top
+    sneaker.rotation.y = radian(-90) + t * 0.01
+}
+
+//Canvas animation loop
+const animate = () => {
+    requestAnimationFrame(animate)
+        // torus.rotation.x += 0.01
+        // torus.rotation.y += 0.005
+        // torus.rotation.z += 0.01
+    renderer.render(scene, camera)
+}
+
+animate()
+
+//Dynamic size
+setInterval(() => {
+    renderer.setClearColor(dark ? 0x000000 : 0xffffff, 1)
+    renderer.setSize((25 * window.innerWidth) / 100, (25 * window.innerWidth) / 100)
+}, 10)
--- public/script.js
+++ public/script.js
@@ -1,14 +1,16 @@
 let dark = true
 let i = 0
 
+//Loop
 setInterval(() => {
-    // White or Dark Mode
+    //360deg reset
     if (i == 360) i = 0
 
     // Apply site wide color scheme
-    document.querySelector(':root').style.setProperty('--color', dark ? "#4a94fa" : "#990099")
-    document.querySelector(':root').style.setProperty('--bgColor', dark ? "#000000" : "#ffffff")
+    document.querySelector(":root").style.setProperty("--color", dark ? "#4a94fa" : "#990099")
+    document.querySelector(":root").style.setProperty("--bgColor", dark ? "#000000" : "#ffffff")
 
+    //Rotate gradient
     document.querySelectorAll("h1").forEach(e => {
         e.style.background = dark ? `linear-gradient(${i}deg, rgba(2, 0, 36, 1) 25%, rgba(2, 0, 36, 1) 50%, #4a94fa 75%, #4a94fa 100%)` : `linear-gradient(${i}deg, rgb(117, 22, 117) 25%, rgb(82, 41, 82) 50%, rgb(75, 23, 75) 75%, rgb(82, 41, 82) 100%)`
         e.style.webkitBackgroundClip = "text"
