ba9303a9260756867e16c17721c29688fe7cf590
DxxxxY <introsdream@gmail.com>
2021-11-29 16:33:54 -0500

    complexified form (added captcha)

--- public/3D.js
+++ public/3D.js
@@ -18,7 +18,7 @@ import { GLTFLoader } from "../utils/GLTFLoader.js"
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.set(0, 0, 0.3)
-const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canvas"), })
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canvas") })
 renderer.setPixelRatio(window.devicePixelRatio)
 const loader = new GLTFLoader()
 let sneaker
