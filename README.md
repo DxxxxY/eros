5b38e411fda083d6290cd4862c79dfb72b5fd802
SuperBluw <36907011+SuperBluw@users.noreply.github.com>
2021-12-02 20:18:52 -0500

    Fixed all navbar problems & Added dark mode
    Fixed all navbar problems & Added dark mode

--- README.md
+++ README.md
@@ -3,8 +3,6 @@ Cleverly designed page for showcasing shoes.
 
 ## TODO:
 - [ ] Complete home page (model + text)
-- [ ] Create guides page
 - [ ] Create media page
 - [ ] Create stores page
 - [ ] Reorganize js and css
-- [ ] Color switch button
--- public/script.js
+++ public/script.js
@@ -1,45 +1,67 @@
+// Dark mode
 let dark = true
+if (localStorage.getItem("current_theme") == "dark") {
+    dark = true
+} else if (localStorage.getItem("current_theme") == "light") {
+    dark = false
+} else {
+    localStorage.setItem("current_theme", dark ? "dark" : "light")
+}
+
+
+
 let i = 0
 
 // Determine which page is being accessed
 var page = window.location.pathname.split("/").pop().split(".html")[0];
 
-let mainArray = ["home", "guides", "media", "stores", "contact", "credits", ""] // Array of the elements that can be highlighted
-
-
-document.querySelectorAll(".hover-underline-animation").forEach( e => {
-    // Check if element is in array and its not a random link
-    if (mainArray.includes(e.textContent.toLowerCase())) {
-
-        // Make element "Not active"
-        e.style.color = dark ? "#0d5ca5" : "#990099"
-
-        // Make specific element active and not get changed by css
-        if (e.textContent.toLowerCase() == page.toLowerCase() || e.textContent.toLowerCase() == "home" && page.toLowerCase() == "index" || e.textContent.toLowerCase() == "home" && page.toLowerCase() == "") {
-            e.style.color = dark ? "#4a94fa" : "#990099"
-            e.id = "active"
-            e.className = ""
-        }
-    }
-})
+// Array of the elements that can be highlighted
+let mainArray = ["home", "guides", "media", "stores", "contact", "credits", ""] 
+let ignoreArrray = [];
+// Set button text
 
 document.querySelectorAll(".hover-underline-animation").forEach(e => {
     // Check if element is in array and its not a random link
     if (mainArray.includes(e.textContent.toLowerCase())) {
         e.addEventListener('mouseover', function() {
-            e.style.color = dark ? "#4a94fa" : "#990099";
+            if (e.id != "active") {
+                e.style.color = dark ? "#4a94fa" : "#990099";
+                ignoreArrray.push(e.innerText)
+            }
         });
         e.addEventListener('mouseleave', function() {
-            e.style.color = dark ? "#0d5ca5" : "#990099";
+            if (e.id != "active") {
+                e.style.color = dark ? "#0d5ca5" : "#990099";
+                ignoreArrray.splice(0, 2)
+            }
         });
     }
 });
-
 
 //Loop
 setInterval(() => {
     //360deg reset
     if (i == 360) i = 0
+
+    document.querySelectorAll(".hover-underline-animation, #active").forEach( e => {
+        // Check if element is in array and its not a random link
+        console.log(ignoreArrray)
+        if (mainArray.includes(e.textContent.toLowerCase()) && (!ignoreArrray.includes(e.innerText))) {
+    
+            // Make element "Not active"
+            e.style.color = dark ? "#0d5ca5" : "#600360"
+    
+            // Make specific element active and not get changed by css
+            if (e.textContent.toLowerCase() == page.toLowerCase() || e.textContent.toLowerCase() == "home" && page.toLowerCase() == "index" || e.textContent.toLowerCase() == "home" && page.toLowerCase() == "") {
+                console.log("changed")
+                e.style.color = dark ? "#4a94fa" : "#990099"
+                e.id = "active"
+                e.className = ""
+            }
+        }
+    })
+    
+    document.getElementById("modeSwitch").innerText = dark ? "Light Mode" : "Dark Mode"
 
     // Apply site wide color scheme
     document.querySelector(":root").style.setProperty("--color", dark ? "#4a94fa" : "#990099")
@@ -52,3 +74,17 @@ setInterval(() => {
     })
     i++
 }, 10)
+
+
+function darkSwitch() {
+    if (localStorage.getItem("current_theme") == "dark") {
+        localStorage.setItem("current_theme", "light")
+        dark = false
+    } else if (localStorage.getItem("current_theme") == "light") {
+        localStorage.setItem("current_theme", "dark")
+        dark = true
+    } else {
+        localStorage.setItem("current_theme", "dark")
+        dark = true
+    }
+}
