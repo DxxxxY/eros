22174290c0c7157ee403d798ddc2e26f4fe316a1
DxxxxY <introsdream@gmail.com>
2021-12-03 12:19:40 -0500

    title, clean and order 3

--- public/script/3D.js
+++ public/script/3D.js
@@ -25,7 +25,7 @@ const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("canva
 renderer.setPixelRatio(window.devicePixelRatio)
 const loader = new GLTFLoader()
 let sneaker
-loader.load(["localhost", "127.0.0.1", ""].includes(window.location.hostname) ? "../utils/model/sneaker/scene.gltf" : "https://dxxxxy.github.io/eros/utils/model/sneaker/scene.gltf", e => {
+loader.load(["localhost", "127.0.0.1", ""].includes(window.location.hostname) ? "../../media/model/sneaker/scene.gltf" : "https://dxxxxy.github.io/eros/media/model/sneaker/scene.gltf", e => {
     scene.add(e.scene)
     sneaker = e.scene
     sneaker.rotation.set(0, radian(-90), radian(-45))
@@ -37,34 +37,14 @@ loader.load(["localhost", "127.0.0.1", ""].includes(window.location.hostname) ? 
         const t = document.body.getBoundingClientRect().top
         sneaker.rotation.y = radian(-90) + t * 0.01
         let value = (t * -100) / window.innerWidth;
-        if (value > 30) {
-            document.querySelector('canvas').className = "canvashover"
-        } else {
-            document.querySelector('canvas').className = ""
-        }
+        if (value > 30) document.querySelector('canvas').className = "canvashover"
+        else document.querySelector('canvas').className = ""
     }
 }, null, e => { console.log(e) })
-
-// Torus
-// const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-// const material = new THREE.MeshStandardMaterial({ color: 0xff6347 })
-// const torus = new THREE.Mesh(geometry, material)
-// scene.add(torus)
-
-// Lights
-const pointLight = new THREE.PointLight(0xffffff) //0x is #
-pointLight.position.set(5, 5, 5)
-const ambientLight = new THREE.AmbientLight(0xffffff) //0x is #
-scene.add(pointLight, ambientLight)
-
-
 
 //Canvas animation loop
 const animate = () => {
     requestAnimationFrame(animate)
-        // torus.rotation.x += 0.01
-        // torus.rotation.y += 0.005
-        // torus.rotation.z += 0.01
     renderer.render(scene, camera)
 }
 
